52fa52320c8b6e95908847cb746c6a2ff7268da7
Jono Finger <jfinger@plos.org>
2015-05-01 16:39:01 -0700

    Working on fix for mogile in docker.

--- docker/tracker/run.sh
+++ docker/tracker/run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 sleep 5
-echo "db_dsn = DBI:mysql:mogilefs:host=$MOGDB_1_PORT_3306_TCP_ADDR" >> mogilefsd.conf
-mogdbsetup --yes --dbhost=$MOGDB_1_PORT_3306_TCP_ADDR --dbname=mogilefs --dbuser=root --dbpassword=''
+echo "db_dsn = DBI:mysql:mogilefs:host=mogdb" >> mogilefsd.conf
+mogdbsetup --yes --dbhost=mogdb --dbname=mogilefs --dbuser=root --dbpassword=''
 useradd -g nogroup -s /bin/false -d /var/mogdata mogilefs
 sudo -u mogilefs mogilefsd -c mogilefsd.conf
-mogadm host add stored --ip=$NODE_1_PORT_7500_TCP_ADDR --port=7500 --status=alive
+echo mogadm host add stored --ip=node --port=7500 --status=alive
+mogadm host add stored --ip=node --port=7500 --status=alive
 mogadm device add stored 1
 mogadm check
 mogadm domain add maindomain
