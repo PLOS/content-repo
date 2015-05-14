23a1611cb928613466fc0ab0f7b142d42ea169e5
Gabriela Filomeno Anaya <gabriela.filomeno@globant.com>
2015-05-14 12:06:34 -0500

    dev-DPRO-842: Code Review

--- test/api/RequestObject/collections_json.py
+++ test/api/RequestObject/collections_json.py
@@ -23,86 +23,85 @@ NOT_FOUND = 404
 
 
 class CollectionsJson(BaseServiceTest):
+  def get_collections(self, **kwargs):
+    """
+    Calls CREPO API to get collections list in a bucket
+    GET /collections ?bucketName={bucketName}...
 
-    def get_collections(self, **kwargs):
-        """
-        Calls CREPO API to get collections list in a bucket
-        GET /collections ?bucketName={bucketName}...
+    :param bucketName, offset, limit, includeDeleted, includePurged, tag
+    """
+    self.doGet('%s' % COLLECTIONS_API, kwargs, DEFAULT_HEADERS)
+    self.parse_response_as_json()
 
-        :param bucketName, offset, limit, includeDeleted, includePurged, tag
-        """
-        self.doGet('%s' % COLLECTIONS_API, kwargs, DEFAULT_HEADERS)
-        self.parse_response_as_json()
+  def post_collections(self, collection_data):
+    """
+    Calls CREPO API to create collection.
 
-    def post_collections(self, collection_data):
-        """
-        Calls CREPO API to create collection.
+    :param collection_data. JSON collection structure
+    """
+    self.doPost('%s' % COLLECTIONS_API, data=json.dumps(collection_data), files=None,
+                headers={'Content-Type': 'application/json'})
+    self.parse_response_as_json()
 
-        :param collection_data. JSON collection structure
-        """
-        self.doPost('%s' % COLLECTIONS_API, data=json.dumps(collection_data), files=None,
-                    headers={'Content-Type': 'application/json'})
-        self.parse_response_as_json()
+  def get_collection(self, bucketName=None, **kwargs):
+    """
+    Calls CREPO API to get a collection by key
+    :param name: bucket name.key
+    """
+    self.doGet('%s/%s' % (COLLECTIONS_API, bucketName), params=kwargs, headers=DEFAULT_HEADERS)
+    self.parse_response_as_json()
 
-    def get_collection(self, bucketName=None, **kwargs):
-        """
-        Calls CREPO API to get a collection by key
-        :param name: bucket name.key
-        """
-        self.doGet('%s/%s' % (COLLECTIONS_API, bucketName), params=kwargs, headers=DEFAULT_HEADERS)
-        self.parse_response_as_json()
+  def get_collection_versions(self, bucketName=None, **kwargs):
+    """
+    Calls CREPO API to get a collection versions
+    :param name: bucket name, key
+    """
+    self.doGet('%s/versions/%s' % (COLLECTIONS_API, bucketName), params=kwargs, headers=DEFAULT_HEADERS)
+    self.parse_response_as_json()
 
-    def get_collection_versions(self, bucketName=None, **kwargs):
-        """
-        Calls CREPO API to get a collection versions
-        :param name: bucket name, key
-        """
-        self.doGet('%s/versions/%s' % (COLLECTIONS_API, bucketName), params=kwargs, headers=DEFAULT_HEADERS)
-        self.parse_response_as_json()
+  def delete_collection(self, bucketName=None, **kwargs):
+    """
+    Calls CREPO API to delete a collection
+    :param name: bucket name.
+    """
+    self.doDelete('%s/%s' % (COLLECTIONS_API, bucketName), params=kwargs, headers=DEFAULT_HEADERS)
 
-    def delete_collection(self, bucketName=None, **kwargs):
-        """
-        Calls CREPO API to delete a collection
-        :param name: bucket name.
-        """
-        self.doDelete('%s/%s' % (COLLECTIONS_API, bucketName), params=kwargs, headers=DEFAULT_HEADERS)
+  @needs('parsed', 'parse_response_as_json()')
+  def verify_get_collections(self):
+    """
+    Verifies a valid response for GET /collections.
+    """
+    self.verify_http_code_is(OK)
 
-    @needs('parsed', 'parse_response_as_json()')
-    def verify_get_collections(self):
-        """
-        Verifies a valid response for GET /collections.
-        """
-        self.verify_http_code_is(OK)
+  @needs('parsed', 'parse_response_as_json()')
+  def verify_get_collection(self, **kwargs):
+    """
+    Verifies a valid response for GET /collection/{bucketName}
+    """
+    self.verify_http_code_is(OK)
+    for k, v in kwargs.items():
+      actual = self.parsed.get_collectionAttribute(k)
+      self.assertEquals(actual, v, '%r is not correct: %r != %r' % (k, v, actual))
 
-    @needs('parsed', 'parse_response_as_json()')
-    def verify_get_collection(self, **kwargs):
-        """
-        Verifies a valid response for GET /collection/{bucketName}
-        """
-        self.verify_http_code_is(OK)
-        for k, v in kwargs.items():
-            actual = self.parsed.get_collectionAttribute(k)
-            self.assertEquals(actual, v, '%r is not correct: %r != %r' % (k, v, actual))
+  def get_object_versions(self, bucketName=None, **kwargs):
+    """
+    Calls CREPO API to get objects list in a bucket
+    GET /objects ?bucketName={bucketName}...
 
-    def get_objects(self, **kwargs):
-        """
-        Calls CREPO API to get objects list in a bucket
-        GET /objects ?bucketName={bucketName}...
+    :param bucketName, kwargs
+    """
+    self.doGet('%s/versions/%s' % (OBJECTS_API, bucketName), params=kwargs, headers=DEFAULT_HEADERS)
+    self.parse_response_as_json()
 
-        :param bucketName, offset, limit, includeDeleted, includePurged, tag
-        """
-        self.doGet('%s' % OBJECTS_API, kwargs, DEFAULT_HEADERS)
-        self.parse_response_as_json()
+  def post_object(self, files=None, **kwargs):
+    """
+    Create a new objects to build collection.
+    """
+    self.doPost('%s' % OBJECTS_API, data=kwargs, files=files, headers=DEFAULT_HEADERS)
+    self.parse_response_as_json()
 
-    def post_object(self, files=None, **kwargs):
-        """
-        Create a new objects to build collection.
-        """
-        self.doPost('%s' % OBJECTS_API, data=kwargs, files=files, headers=DEFAULT_HEADERS)
-        self.parse_response_as_json()
-
-    def delete_object(self, bucketName=None, **kwargs):
-        """
-        Delete test objects
-        """
-        self.doDelete('%s/%s' % (OBJECTS_API, bucketName), params=kwargs, headers=DEFAULT_HEADERS)
+  def delete_object(self, bucketName=None, **kwargs):
+    """
+    Delete test objects
+    """
+    self.doDelete('%s/%s' % (OBJECTS_API, bucketName), params=kwargs, headers=DEFAULT_HEADERS)
