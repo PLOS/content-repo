5c3bd91e1af4882b267f88146c310141ac731b37
Erik Hetzner <ehetzner@plos.org>
2019-11-12 13:50:35 -0800

    AMBR-1075: s3-migrate: add AWS lambda processor

---
+++ s3migrate/lambda.py
@@ -0,0 +1,20 @@
+import json
+import os
+from shared import MogileFile, make_bucket_map
+import pymogilefs
+import boto3
+
+def process(event, context):
+    bucket_map = make_bucket_map(os.environ["BUCKETS"])
+    mogile_client = pymogilefs.client.Client(
+        trackers=os.environ['MOGILE_TRACKERS'].split(','),
+        domain='plos_repo')
+    s3_client = boto3.resource('s3')
+
+    for record in event['Records']:
+        mogile_file = MogileFile.from_json(record["body"])
+        mogile_file.migrate(
+            mogile_client,
+            s3_client,
+            bucket_map)
+
