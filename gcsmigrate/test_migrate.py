1481e0deef2d03c70551a44bc2973e88d60cdffe
Erik Hetzner <ehetzner@plos.org>
2020-07-06 17:11:11 -0700

    ENG-285 gcsmigrate: drop all references to boto

--- gcsmigrate/test_migrate.py
+++ gcsmigrate/test_migrate.py
@@ -2,7 +2,6 @@ import tempfile
 from unittest.mock import Mock
 
 import pytest
-from botocore.exceptions import ClientError
 
 from .shared import MogileFile, make_bucket_map,\
     md5_fileobj_hex, sha1_fileobj_hex, md5_fileobj_b64, sha1_fileobj_b64, \
