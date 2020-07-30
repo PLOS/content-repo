3bbf8b87db41f00dac6c72b39e1dbcd86fe03779
Shawn Gatchell <shawng03@gmail.com>
2020-07-30 12:55:18 -0700

    ENG-285 migrate fids counting to firestore

--- gcsmigrate/fids.py
+++ gcsmigrate/fids.py
@@ -1,70 +1,16 @@
 import os
-import threading
 
-import boto3
+from google.cloud import firestore
 
-
-class FidThread(threading.Thread):
-    """Thread worker for scanning fids from DynamoDB."""
-
-    def __init__(self, fids, lock, segment, total_segments, *args, **kwargs):
-        """Initialize this thread."""
-        super(FidThread, self).__init__(*args, **kwargs)
-        self.client = boto3.client("dynamodb")
-        self.fids = fids
-        self.lock = lock
-        self.segment = segment
-        self.total_segments = total_segments
-
-    def run(self):
-        """Run this thread."""
-        kwargs = {"TableName": os.environ["DYNAMODB_TABLE"],
-                  "AttributesToGet": ["fid"],
-                  "Segment": self.segment,
-                  "TotalSegments": self.total_segments}
-        response = self.client.scan(**kwargs)
-        while True:
-            values = [int(item["fid"]["N"])
-                      for item in response["Items"]]
-            with self.lock:
-                self.fids.update(values)
-            if "LastEvaluatedKey" in response:
-                response = self.client.scan(
-                    ExclusiveStartKey=response["LastEvaluatedKey"], **kwargs)
-            else:
-                break
-
-    @classmethod
-    def start_pool(cls, fids):
-        """Run threads on the data provided in the queue.
-
-        Returns a list of threads to pass to `finish_pool` later.
-        """
-        lock = threading.Lock()
-        total_segments = 100
-        threads = []
-        for segment in range(total_segments):
-            thread = cls(fids=fids, lock=lock, segment=segment,
-                         total_segments=total_segments)
-            threads.append(thread)
-            thread.start()
-        return threads
-
-    @staticmethod
-    def finish_pool(threads):
-        """Wait until queue is empty and threads have finished processing."""
-        # stop workers
-        for thread in threads:
-            thread.join()
-
+COLLECTION_NAME = os.environ["COLLECTION_NAME"]
 
 def main():
     """Print all processed fids to stdout."""
-    fids = set()
-    threads = FidThread.start_pool(fids=fids)
-    FidThread.finish_pool(threads)
-    for fid in fids:
-        print(fid)
+    firestore_client = firestore.Client()
+    collection = firestore_client.collection(COLLECTION_NAME)
+    docs = collection.select(['fid']).stream()
+    for doc in docs:
+        print(doc.fid)
 
 
 if __name__ == "__main__":
