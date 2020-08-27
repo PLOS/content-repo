99b496dcea5526600460016fbca3602ea9eddcc0
Erik Hetzner <ehetzner@plos.org>
2020-08-27 15:21:35 -0700

    ENG-285 gcsmigrate: add maybe_db context manager

--- gcsmigrate/verify.py
+++ gcsmigrate/verify.py
@@ -1,5 +1,6 @@
 import dbm.gnu
 import os
+from contextlib import contextmanager
 
 from google.cloud import storage
 from tqdm import tqdm
@@ -7,10 +8,26 @@ from tqdm import tqdm
 from shared import get_mogile_files_from_database, make_bucket_map
 
 
+@contextmanager
+def maybe_db(dbpath):
+    if os.path.exists(dbpath):
+        yield None
+    else:
+        with dbm.gnu.open(dbpath, "cf") as db:
+            try:
+                yield db
+                db.sync()
+            except:
+                # Clean up db if there was an error
+                os.unlink(dbpath)
+
+
 def load_bucket(bucket_name):
     gcs_client = storage.Client()
-    with tqdm(desc=f"{bucket_name} list") as pbar:
-        with dbm.gnu.open(f"{bucket_name}.db", "cf") as db:
+    with maybe_db(f"{bucket_name}.db") as db:
+        if db is None:
+            return
+        with tqdm(desc=f"{bucket_name} list") as pbar:
             for page in gcs_client.list_blobs(
                 bucket_name, prefix="", delimiter="/"
             ).pages:
@@ -20,16 +37,17 @@ def load_bucket(bucket_name):
                     # keeping track of the keys.
                     db[key] = ""
                     pbar.update()
-            db.sync()
-            return True
+
 
 def load_mogile():
-    with dbm.gnu.open("mogile.db", "cf") as db:
+    with maybe_db("mogile.db") as db:
+        if db is None:
+            return
         for mogile_file in tqdm(
-                get_mogile_files_from_database(os.environ["MOGILE_DATABASE_URL"])
+            get_mogile_files_from_database(os.environ["MOGILE_DATABASE_URL"])
         ):
-            db[f"{mogile_file.sha1sum}_{mogile_file.mogile_bucket}"] = ""
-        db.sync()   
+            db[f"{mogile_file.fid}_{mogile_file.sha1sum}_{mogile_file.mogile_bucket}"] = ""
+
 
 def main():
     """
@@ -48,16 +66,19 @@ def main():
     dbs = {bucket: dbm.open(f"{bucket}.db") for bucket in buckets}
     load_mogile()
     mogile_db = dbm.open("mogile.db")
-    key = mogile_db.firstkey()
-    while key is not None:
-        sha1sum, mogile_bucket = key.decode("utf-8").split("_")
-        if mogile_bucket in ignore_buckets:
-            continue
-        remote_bucket = bucket_map[mogile_bucket]
-        assert sha1sum in dbs[remote_bucket], f"{sha1sum} not in {remote_bucket}"
-
-        key = mogile_db.nextkey(key)
-
+    with tqdm(desc="verifying") as pbar:
+        key = mogile_db.firstkey()
+        while key is not None:
+            fid, sha1sum, mogile_bucket = key.decode("utf-8").split("_")
+            key = mogile_db.nextkey(key)
+            pbar.update()
+            if mogile_bucket in ignore_buckets:
+                continue
+            # hack!
+            if fid > 15000000:
+                continue
+            remote_bucket = bucket_map[mogile_bucket]
+            assert sha1sum in dbs[remote_bucket], f"{sha1sum} not in {remote_bucket}"
 
 if __name__ == "__main__":
     main()
