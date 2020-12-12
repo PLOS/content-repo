d903c04eb9f6dc20b837c80447240811c797d2cf
Erik Hetzner <ehetzner@plos.org>
2020-12-11 20:24:28 -0800

    ENG-462 gcsmigrate: verify does not use gdbm for mogile

--- gcsmigrate/verify.py
+++ gcsmigrate/verify.py
@@ -9,25 +9,9 @@ import dbm.gnu
 import os
 import sys
 
-from google.cloud import storage
 from tqdm import tqdm
 
-from shared import get_mogile_files_from_database, make_bucket_map, open_db, load_bucket
-
-
-def load_mogile():
-    dbpath = "mogile.db"
-    if os.path.exists(dbpath):
-        return
-    with open_db("mogile.db") as db:
-        for mogile_file in tqdm(
-            get_mogile_files_from_database(os.environ["MOGILE_DATABASE_URL"])
-        ):
-            if mogile_file.skip:
-                continue
-            db[
-                f"{mogile_file.fid}_{mogile_file.sha1sum}_{mogile_file.mogile_bucket}"
-            ] = ""
+from shared import get_mogile_files_from_database, make_bucket_map, load_bucket
 
 
 def main():
@@ -37,21 +21,18 @@ def main():
     for bucket in buckets:
         load_bucket(bucket, f"{bucket}.db")
     dbs = {bucket: dbm.open(f"{bucket}.db") for bucket in buckets}
-    load_mogile()
-    mogile_db = dbm.open("mogile.db")
     failed = False
-    with tqdm(desc="verifying") as pbar:
-        key = mogile_db.firstkey()
-        while key is not None:
-            fid, sha1sum, mogile_bucket = key.decode("utf-8").split("_")
-            key = mogile_db.nextkey(key)
-            pbar.update()
-            if mogile_bucket in ignore_buckets:
-                continue
-            remote_bucket = bucket_map[mogile_bucket]
-            if sha1sum not in dbs[remote_bucket]:
-                print(fid)
-                failed = True
+    for mogile_file in tqdm(
+        get_mogile_files_from_database(os.environ["MOGILE_DATABASE_URL"])
+    ):
+        if mogile_file.skip:
+            continue
+        if mogile_file.mogile_bucket in ignore_buckets:
+            continue
+        remote_bucket = bucket_map[mogile_file.mogile_bucket]
+        if mogile_file.sha1sum not in dbs[remote_bucket]:
+            print(mogile_file.fid)
+            failed = True
     sys.exit(1 if failed else 0)
 
 
